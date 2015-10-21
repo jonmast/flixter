0d389d497f377b68850c35be5eea3cac95a0988c
Jonathan Mast <jon@padorpers.com>
2015-10-21 08:55:04 -0400

    Use carrierwave-aws instead of fog
    Fog has some issues with video uploads

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,10 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET']
+  config.storage = :aws
+  config.aws_bucket  = 'nomster-jon'
+  config.aws_acl = 'public-read'
+  config.aws_credentials = {
+      :access_key_id     => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET'],
+      :region            => 'us-east-1'
   }
-  config.fog_directory  = 'nomster-jon'
 end
